ba1fd638fa8fc5dce233b5a6172f35a9f94fe57d
Jacopo1891 <jacopo1891@gmail.com>
2023-06-17 02:59:10 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# DNSUpdater
+Script to update dns record with current public ip.
+
+# Required
+- [Lexicon](https://github.com/AnalogJ/lexicon)
+- cURL
+
+# Options
+Debug mode to see few logs
+```python3 dnsUpdater.py --debug```
+
+
---
+++ dnsUpdater.py
@@ -0,0 +1,56 @@
+import os
+import sys
+from lexicon.client import Client
+from lexicon.config import ConfigResolver
+from lexicon.providers.ovh import Provider
+
+from config import *
+
+DEBUG = "--debug" in sys.argv
+
+def check_config():
+    if recordUrl == "":
+        print("Record url is empty: cannot start.")
+        sys.exit(1)
+    if LexiconConfig == {}:
+        print("Lexicon's config is empty: cannot start.")
+        sys.exit(1)
+
+def print_debug_info(current_ip_address, record_ip_address, result):
+    print("Current public IP:", current_ip_address)
+    print("Current record IP:", record_ip_address)
+    if result:
+        print("IP updated")
+    if result is None:
+        print("IP is up to date")
+    else:
+        print("Something went wrong")
+
+def get_current_ip_address():
+    return os.popen('curl -s https://api.ipify.org').read().strip()
+
+def get_vpn_ip_address(provider):
+    record_ip_address_response = provider.list_records(name=recordUrl)
+    return record_ip_address_response[0]["content"]
+
+def update_record_vpn(provider, current_ip_address):
+    return provider.update_record(name=recordUrl, content=current_ip_address)
+
+def main():
+    check_config()
+    provider = Provider(LexiconConfig)
+    provider.authenticate()
+
+    current_ip_address = get_current_ip_address()
+    record_ip_address = get_vpn_ip_address(provider)
+
+    if record_ip_address != current_ip_address:
+        result = update_record_vpn(provider, current_ip_address)
+    else:
+        result = None
+        
+    if DEBUG:
+        print_debug_info(current_ip_address, record_ip_address, result)
+
+if __name__ == "__main__":
+    main()
